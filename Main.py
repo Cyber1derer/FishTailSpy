fe1e90525e189d6a68b0f6524d56803201e7c909
Denis V <Vorkutov.Denis.A@yandex.ru>
2024-05-14 03:06:24 +0300

    upd

--- Main.py
+++ Main.py
@@ -7,7 +7,7 @@ from icecream import ic
 import math
 
 
-#TODO Зафиксировать гистограмму после 1 выранвнивания
+#TODO Зафиксировать гистограмму после 1 выранвнивания; Найти соединение по количеству точек хвоста
 
 #global useEqualize, blurSize, CannyThesh1,CannyThesh2
 # Initialize default parameter values
@@ -20,9 +20,29 @@ frameCount = 1
 ret = True
 prew_lengFish = 0
 cap_start_msec = 21_000
-cap_end_msec = 24_000
+cap_end_msec = 23_000
 frame_end = (cap_end_msec - cap_start_msec) /20 
-import numpy as np
+cdf = None
+def find_farthest_point2(UpPx, x, y, vx, vy):
+    max_distance = 0
+    farthest_point = None
+
+    for point in UpPx:
+        # Вычислить расстояние между текущей точкой и заданной точкой (x, y)
+        distance = math.sqrt((point[0] - x) ** 2 + (point[1] - y) ** 2)
+
+        # Проекция точки на прямую vx, vy
+        projection_x = (point[0] * vx + point[1] * vy) / (vx ** 2 + vy ** 2) * vx
+        projection_y = (point[0] * vx + point[1] * vy) / (vx ** 2 + vy ** 2) * vy
+
+        # Вычислить расстояние между проекцией и заданной точкой (x, y) вдоль прямой vx, vy
+        projected_distance = abs(projection_x - x) if vx != 0 else abs(projection_y - y)
+
+        if projected_distance > max_distance:
+            max_distance = projected_distance
+            farthest_point = point
+
+    return farthest_point
 
 def find_farthest_point(vx, vy, x, y, UpPx):    
     # Вычисляем направляющий вектор прямой
@@ -43,7 +63,7 @@ def find_farthest_point(vx, vy, x, y, UpPx):
     idx = np.argmax(np.abs(projections))
     
     # Возвращаем наиболее удаленную точку
-    return points[idx]
+    return points[idx] if idx < len(points) else None
 
 def find_close_point_to_line(vx, vy, x, y, UpPx):    
     # Вычисляем направляющий вектор прямой
@@ -201,7 +221,28 @@ def on_trackbar(val):
     CannyThresh1 = cv2.getTrackbarPos('CannyTresh1', "Canny viewUp Video")
     CannyThresh2 = cv2.getTrackbarPos('CannyTresh2', "Canny viewUp Video")
     blurSize = cv2.getTrackbarPos('BlurGaus', "Canny viewUp Video")
+
+
+
+def HistMod(img,flag = False):
+    global cdf
+    if flag:
+        cdf = np.load('cdf_values.npy')
+        '''
+        hist,bins = np.histogram(img.flatten(),256,[0,256])
+        cdf = hist.cumsum()
+        cdf_m = np.ma.masked_equal(cdf,0)
+        cdf_m = (cdf_m - cdf_m.min())*255/(cdf_m.max()-cdf_m.min())
+        cdf = np.ma.filled(cdf_m,0).astype('uint8')
+        np.save('cdf_values.npy', cdf)
+        '''
+
+    img2 = cdf[img]
+    return img2
+
+
 # Create window and trackbars
+cv2.namedWindow("Source Up", cv2.WINDOW_NORMAL)
 cv2.namedWindow("Canny viewUp Video")
 cv2.createTrackbar("CannyTresh1", "Canny viewUp Video" , CannyThresh1, 500, on_trackbar)
 cv2.createTrackbar("CannyTresh2", "Canny viewUp Video" , CannyThresh2, 500, on_trackbar)
@@ -221,6 +262,10 @@ data = np.load("camera_params.npz")
 mtx = data['mtx']
 dist = data['dist']
 
+#cap.set(cv2.CAP_PROP_POS_FRAMES, 1167)
+clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+
+CalHist = True
 
 while ret:
     ret, frame = cap.read()
@@ -243,12 +288,31 @@ while ret:
     # Convert to grayscale and equalize histogram
     viewUp = cv2.cvtColor(viewUp, cv2.COLOR_BGR2GRAY)
     if useEqualize:
-        viewUp = cv2.equalizeHist(viewUp)
+        viewUp = clahe.apply(viewUp)
+        #viewUp = cv2.equalizeHist(viewUp)
+        #viewUp = HistMod2(viewUp,CalHist)
+        CalHist = False
+        #cv2.imwrite(viewUp)
     
     #viewUp = cv2.morphologyEx(viewUp, cv2.MORPH_OPEN, (15,15))
 
+
+
+
+  
+
+
+
+
     upEdges = cv2.Canny(viewUp, CannyThresh1,CannyThresh2,None,3,False)
     upPxEdges = np.argwhere(upEdges == 255)
+
+    #KeyPoints
+    sift = cv2.SIFT_create()
+    kp = sift.detect(upEdges,None)
+    img=cv2.drawKeypoints(viewUp,kp,viewUpSource)
+    #cv2.imwrite('sift_keypoints.jpg',viewUpSource)
+
 
     if upPxEdges.size == 0:
         print("Edges not found")
@@ -256,7 +320,6 @@ while ret:
         upPxEdges = upPxEdges[:, [1, 0]]
         
         #Find end's points===========
-        # Returned 420 points in testing
         hull = ConvexHull(upPxEdges)
         # Extract the points forming the hull
         hullpoints = upPxEdges[hull.vertices,:]
@@ -273,11 +336,18 @@ while ret:
         if (lengFish > prew_lengFish*1.2 or lengFish < prew_lengFish*0.8)  and prew_lengFish != 0:
             print("False corn")
         else:
+            
+            # Display images
+            #cv2.imshow('Canny viewUp Video', upEdges)
+            #cv2.imshow('viewUp Video', viewUp)
+            #cv2.imshow('Source Up', viewUpSource)
+            cv2.waitKey(1)
             prew_lengFish = lengFish
                     #Print them
             #print("max dist points ",  [hullpoints[bestpair[0]],hullpoints[bestpair[1]]])
-            cv2.circle(viewUpSource,(hullpoints[bestpair[0]] ), 2,(255,0,255), -1)
-            cv2.circle(viewUpSource,(hullpoints[bestpair[1]] ), 2,(255,255,255), -1)
+            #Дальнейшие точки друг от друга
+            #cv2.circle(viewUpSource,(hullpoints[bestpair[0]] ), 2,(255,0,255), -1)
+            #cv2.circle(viewUpSource,(hullpoints[bestpair[1]] ), 2,(255,255,255), -1)
             #cv2.circle(viewUpSource,(column_mean[0], column_mean[1]), 8,(255,0,255), -1) 
 
 
@@ -328,14 +398,20 @@ while ret:
             tailFishContur = filter_points_between(upPxEdges, inflectionPoint, (int(tailRange_x), int(tailRange_y)) )
             headFishContur = filter_points_between(upPxEdges, inflectionPoint, (int(headRange_x), int(headRange_y)) )
 
-            NoseFish = find_farthest_point(vx, vy,x,y, headFishContur)
-   
+            #NoseFish = find_farthest_point(vx, vy,x,y, headFishContur)
+
             
             closest_pairs = find_closest_pairs(headFishContur,tailFishContur )
             #ic (closest_pairs)
             realInflectionPoint = find_midpoint(closest_pairs)
+            ic(cap.get(cv2.CAP_PROP_POS_FRAMES))
             NoseFish = find_farthest_point(vx, vy,x,y, headFishContur)
-
+            #NoseFish = find_farthest_point2(headFishContur, vx, vy,x,y)
+            if NoseFish is None:
+                print("Bad Nose")
+                continue
+            #Разкоменть
+            #cv2.circle(viewUpSource,NoseFish, 5,(0,0,255), -1) #Отрисовка поправленной точки стыковки хвоста
 
 
 
@@ -411,7 +487,8 @@ while ret:
             right_x = cols - 1
             left_y = int(k * left_x + b)
             right_y = int(k * right_x + b)
-            #cv2.line(viewUpSource, (left_x, left_y), (right_x, right_y), (0, 0, 255), 2) #Красная ось симметрии хвоста
+            #Разкоменть
+            #cv2.line(viewUpSource, (left_x, left_y), (right_x, right_y), (255, 0, 255), 2) #Красная ось симметрии хвоста
             #"""
 
             TailPointIndx = find_closest_points_to_line2(tailFishContur,vx, vy,x,y )
@@ -419,16 +496,21 @@ while ret:
             TailPoint = TailPoint[0]
 
             #ic(TailPoint)
+
+            #Раcкоменть ''
+            '''
             for x_t, y_t in tailFishContur:
                 cv2.circle(viewUpSource,( x_t, y_t), 1,(0,0,255), -1)
 
             for x_h, y_h in headFishContur:
                 cv2.circle(viewUpSource,( x_h, y_h), 1,(255,0,0), -1)
+            '''
 
             #cv2.circle(viewUpSource, realInflectionPoint , 2,(0,255,0), -1)
-            cv2.circle(viewUpSource, TailPoint , 1,(0,255,0), -1)
-            cv2.line(viewUpSource, NoseFish,realInflectionPoint,(0,255,255), 2 )
-            cv2.line(viewUpSource, realInflectionPoint,TailPoint,(0,255,0), 2 )
+            #Разкоменть 3 строки
+            #cv2.circle(viewUpSource, TailPoint , 1,(0,255,0), -1)
+           # cv2.line(viewUpSource, NoseFish,realInflectionPoint,(0,255,255), 2 )
+            #cv2.line(viewUpSource, realInflectionPoint,TailPoint,(0,255,0), 2 )
             tailAngle = calculate_angle(NoseFish, realInflectionPoint,TailPoint)
             print("Угол: ", tailAngle)
             '''
@@ -487,7 +569,7 @@ while ret:
         cap.set(cv2.CAP_PROP_POS_MSEC , cap_start_msec)
         frameCount = 1
     #Exit loop if 'q' is pressed
-    if cv2.waitKey(1000) == ord('q'):
+    if cv2.waitKey(0) == ord('q'):
         break
 
 cap.release()
